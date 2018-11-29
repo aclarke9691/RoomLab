a59aedfe1a3f0d688fb34e594927e472ecd677a6
aclarke9691 <aclarke9691@bths.edu>
2018-11-29 15:19:09 -0500

    done

---
+++ src/Rooms/Kitchen.java
@@ -0,0 +1,21 @@
+package Rooms;
+import Game.Runner;
+import People.Person;
+
+public class Kitchen extends Room {
+
+    public Kitchen(int xx, int yy)
+    {
+        super(xx,yy);
+    }
+
+    public void enterRoom(Person p)
+    {
+        occupant=p;
+        p.setxLoc(this.xLoc);
+        p.setyLoc(this.yLoc);
+        System.out.println("You are now in the kitchen. Move anywhere to leave a kitchen.");
+
+    }
+
+}
