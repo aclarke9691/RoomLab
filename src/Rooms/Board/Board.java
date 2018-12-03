efaa1a9d519810ea64cbd5dea8f15a0b4a9f0df5
aclarke9691 <aclarke9691@bths.edu>
2018-12-03 14:50:38 -0500

    skldznf

---
+++ src/Rooms/Board/Board.java
@@ -0,0 +1,17 @@
+package Rooms.Board;
+
+public class Board extends Room {
+    public class Board {
+        private Room[][] House;
+
+        public Board(room[][] House)
+        {
+            this.House=House;
+        }
+
+        public void createHouse(String[][] House)
+        {
+            House= [10][10];
+        }
+    }
+}
