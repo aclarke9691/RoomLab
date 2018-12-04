81063654566646ca54cd65afe880472776a749d7
aclarke9691 <aclarke9691@bths.edu>
2018-12-04 14:47:24 -0500

    ugghhhgghg

---
+++ src/Board/Board.java
@@ -0,0 +1,23 @@
+package Board;
+import Rooms.Room;
+
+public class Board {
+    private Room[][] House;
+    public Board(Room[][] House)
+    {
+        this.House=House;
+    }
+    public void createHouse(Room[][] House)
+    {
+        System.out.print(House);
+        //int i=0;
+       // int j=0;
+        //while(i<House.length)
+        //{
+            //while (j<House[i].length)
+            //{
+            //    House[i][j]=
+            //}
+       // }
+    }
+}
