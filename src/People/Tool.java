c83d862e4269e487425bd6106316876ac7cca385
aclarke9691 <aclarke9691@bths.edu>
2018-12-12 23:49:32 -0500

    jgbhnk

--- src/People/Tool.java
+++ src/People/Tool.java
@@ -30,6 +30,7 @@ public abstract class Tool {
             }
         }
     }
+    public String name;
 
 
 }
--- src/Rooms/SpecialRoom2.java
+++ src/Rooms/SpecialRoom2.java
@@ -9,7 +9,7 @@ public class SpecialRoom2 extends Room {
     private Tool Tools[]; //does this work
     private int i;
 
-    public SpecialRoom2(int xx, int yy, Tool Tools[], i) {
+    public SpecialRoom2(int xx, int yy, Tool Tools[], int i) {
         super(xx, yy);
         this.Tools = Tools;
         this.i=i;
@@ -24,13 +24,15 @@ public class SpecialRoom2 extends Room {
         System.out.println("You arrive upon a messy bedroom and out of the corner of your eye, you see something sparkling in the back of the room. Should you walk over there to go see what it is?");
         if (response.equals("yes") || response.equals("Yes")) {
             System.out.println("You pick up the teddy bear and notice that there is a hole in the back of the bear. You reach into it and find a pair of keys. You put them into your pocket and exit the room.");
-            Tools[i]= "keys";
-        } else {
+            Tools[i].name= "keys"; //how do i get the tool class to accept string
+            i=i+1;
+        }
+        else {
             if (response.equals("no") || response.equals("No")) {
-                System.out.println("Okay, let's leave the room.");
-            } else {
-                System.out.println("Please say yes or no.");
-            }
+               System.out.println("Okay, let's leave the room.");}
+            //} else {
+               // System.out.println("Please say yes or no.");
+           // }
             System.out.println("Move anywhere to leave the room.");
         }
     }
